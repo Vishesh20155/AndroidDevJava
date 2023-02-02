faac01d1b55e4a9fca151a03db98376b6c6a1743
Vishesh Rangwani <vishesh20155@iiitd.ac.in>
2023-02-03 00:52:17 +0530

    Added Toast

--- app/src/main/java/com/example/myfirstapp/SecondFragment.java
+++ app/src/main/java/com/example/myfirstapp/SecondFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -39,6 +40,7 @@ public class SecondFragment extends Fragment {
         binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Toast.makeText(getContext(), "Hello", Toast.LENGTH_SHORT).show();
                 NavHostFragment.findNavController(SecondFragment.this)
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
